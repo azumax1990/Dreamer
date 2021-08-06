a81fcc8fa688df4d8f8f0b8e1f7b49baef62456a
谷村有寿麻 <t.azumax0502@gmail.com>
2021-08-07 01:58:36 +0900

    Header components実装

---
+++ frontend/src/components/templates/HeaderLayout.tsx
@@ -0,0 +1,15 @@
+import React, { VFC, ReactNode, memo } from 'react';
+import { Header } from '../organisms/Header'
+
+type Props = {
+  children: ReactNode;
+}
+export const HeaderLayout: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+})
