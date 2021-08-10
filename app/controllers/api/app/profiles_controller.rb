7243217dcd89064ee84e1da6f5e6c8a2272b27e3
谷村有寿麻 <t.azumax0502@gmail.com>
2021-08-10 21:00:14 +0900

    profiles_controller変更

--- app/controllers/api/app/profiles_controller.rb
+++ app/controllers/api/app/profiles_controller.rb
@@ -1,12 +1,22 @@
 class ::Api::App::ProfilesController < ApplicationController
   def show
-    
+    profile = current_api_user.profile
+    render json: profile
   end
 
   def edit
+    profile = current_api_user.prepare_profile
   end
 
   def updata
+    profile = current_api_user.prepare_profile
+    profile.assign_attributes(profile_params)
+    profile.save
+    render json: { status: "ok"}
+  end
 
+  private
+  def profile_params
+    params.require(:profile).permit(:name, :age, :gender, :tall, :prefecture, :introduction, :avatars: [])
   end
 end
