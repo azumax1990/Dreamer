9735845198ab21519fe8be15a24d8d4f8909ab32
谷村有寿麻 <t.azumax0502@gmail.com>
2021-08-23 20:21:40 +0900

    active strage api作成

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,10 @@
 class Profile < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  
   belongs_to :user
-  has_many_attached :avatars
+  has_one_attached :avatar
+
+  def avatar_url
+    avatar.attached? ? url_for(avatar) : nil
+  end
 end
