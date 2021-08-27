3c7c1ee99fbbffefd2e816696e4fcef788d1e320
谷村有寿麻 <t.azumax0502@gmail.com>
2021-08-27 20:36:39 +0900

    companyに関するprofile機能の追加

--- frontend/src/components/pages/EditProfile.tsx
+++ frontend/src/components/pages/EditProfile.tsx
@@ -1,21 +1,23 @@
-import React, { ChangeEvent, memo, useContext, useState, useEffect,  VFC } from 'react'
+import React, { ChangeEvent, memo, useContext, useState, useEffect, useCallback, VFC } from 'react'
 import styled from 'styled-components'
 import { useHistory } from 'react-router-dom'
 
 import { LoginUserContext } from '../../App'
-
 import { getEditProfile, PostProfile } from '../../api/profile'
-
-import { ages } from '../../data/age'
-import { genders } from '../../data/gender'
-import { heights } from '../../data/height'
-import { prefectures } from '../../data/prefecture'
 import { ParamsProfile } from '../../types'
+
+import { EditPlayerProfile } from '../organisms/edit/EditPlayerProfile'
+import { EditCompanyProfile } from '../organisms/edit/EditCompanyProfile'
+import { useEditProfile } from '../../hooks/useEditProfile'
+import MediaQuery from 'react-responsive'
 
 const SignUpWrapper = styled.div`
   background-color: #F5F5F5;
   padding: 40px 0;
   min-height: 100vh;
+`
+const SignUpWrap = styled.div`
+  background-color: #F5F5F5;
 `
 const PageTittle = styled.h1`
   font-size: 30px;
@@ -30,42 +32,12 @@ const SignUpContainer = styled.div`
   margin: 0 auto;
   box-shadow:  0 0 3px gray;
 `
-const LabelTag = styled.label`
-  font-size: 20px;
-`
-const InputContainer = styled.div`
-  padding-bottom: 25px;
-`
-const InputTag = styled.input`
-  width: 100%;
-  font-size: 18px;
-  box-sizing:border-box;
-  padding: 8px;
-  margin-top: 10px;
-  border-color: #f5f5f5;
-  outline: none;
-  box-shadow:  0 0 3px gray;
-`
-const TextAreaTag = styled.textarea`
-  width: 100%;
-  height: 200px;
-  font-size: 15px;
-  box-sizing:border-box;
-  padding: 8px;
-  margin-top: 10px;
-  border-color: #f5f5f5;
-  outline: none;
-  box-shadow:  0 0 3px gray;
-`
-const SelectTag = styled.select`
-  width: 100%;
-  padding: 10px;
-  margin-top: 10px;
-  box-shadow: 0 0 3px gray;
-  outline: none;
-  font-size: 16px;
-`
-const OptionTag = styled.option`
+const SignUpContain = styled.div`
+  width: auto;
+  background-color: #fff;
+  padding: 50px 40px;
+  margin: 0 auto;
+  // box-shadow:  0 0 3px gray;
 `
 const SubmitButton = styled.button`
   width: 100%;
@@ -81,120 +53,126 @@ type Props = {
 }
 export const EditProfile: VFC<Props> = memo((props) => {
   const { id } = props;
-
-  const [name, setName]                 = useState('')
-  const [avatar, setAvatar]             = useState({data: "", name: ""})
-  const [age, setAge]                   = useState<number>()
-  const [gender, setGender]             = useState('')
-  const [tall, setTall]                 = useState<number>()
-  const [prefecture, setPrefecture]     = useState('')
-  const [introduction, setIntroduction] = useState('')
-
   const { currentUser } = useContext(LoginUserContext)
-  const history = useHistory()
-  
-  const params: ParamsProfile = {
-    id: currentUser?.id,
-    name: name,
-    age: age, 
-    gender: gender,
-    tall: tall, 
-    prefecture: prefecture,
-    introduction: introduction, 
-    avatar: avatar,
-    
-  }
+
+  // カスタムフック
+  const { job,
+          name,
+          setName,
+          avatar,
+          age,
+          setAge,
+          gender,
+          setGender,
+          tall,
+          setTall,
+          prefecture,
+          setPrefecture,
+          introduction,
+          setIntroduction,
+          company,
+          setCompany,
+          description,
+          setDescription,
+          onClickPostProfile,
+          onChangeImage,
+          onChangeJob,
+          onChangeName,
+          onChangeAge,
+          onChangeGender,
+          onChangeTall,
+          onChangePrefecture,
+          onChangeIntroduction,
+          onChangeCompany,
+          onChangeDescription} = useEditProfile()
 
   // 編集プロフィール取得
   useEffect(() => {
-    getEditProfile(currentUser?.id || 19)
+    getEditProfile(currentUser?.id || id)
     .then((res) => {
-      setName(res.data.name ?? '')
-      setAge(res.data.age ?? '')
-      setGender(res.data.gender ?? '')
+      setName(res.data.name)
+      setAge(res.data.age)
+      setGender(res.data.gender)
       setTall(res.data.tall)
-      setPrefecture(res.data.prefecture ?? '')
-      setIntroduction(res.data.introduction ?? '')
+      setPrefecture(res.data.prefecture)
+      setIntroduction(res.data.introduction)
+      setCompany(res.data.company)
+      setDescription(res.data.decription)
     })
     .catch(() => alert('エラー'))
   }, [currentUser])
 
-  // プロフィールデータ送信
-  const onClickPostProfile = () => {
-    PostProfile(params)
-    .then((res) => (
-      history.push(`/user/${currentUser?.id}/profile`)
-    ))
-    .catch(() => alert("プロフィールを更新できませんでした"))
-  }
-
-  // プロフィール写真更新
-  const uploadImage = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const reader = new FileReader()
-      const file = e.target.files[0];
-      reader.onload = () => { 
-        setAvatar({
-          data: reader.result as string,
-          name: file.name
-        })
-      }
-      reader.readAsDataURL(file)
-    }
-  }
-
   return (
     <>
-      <SignUpWrapper>
-        <PageTittle>プロフィール</PageTittle>
-        <SignUpContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formName">名前</LabelTag>
-            <InputTag id="formName" value={name} onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
-          </InputContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formAvatar">写真</LabelTag>
-            <InputTag type="file" id="formAvatar" name="avatar" onChange={(e: ChangeEvent<HTMLInputElement>) => (uploadImage(e))}/>
-          </InputContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formAge">年齢</LabelTag>
-            <SelectTag id="formAge" value={age} onChange={(e: ChangeEvent<{ value: unknown }>) => (setAge(e.target.value as number))}>
-              {ages.map((age: string, index: number) => (
-                <OptionTag key={index} value={age}>{age}</OptionTag>
-              ))}
-            </SelectTag>
-          </InputContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formGender">性別</LabelTag>
-            <SelectTag id="formGender" value={gender} onChange={(e: ChangeEvent<HTMLSelectElement>) => (setGender(e.target.value))}>
-              {genders.map((gender: string, index: number) => (
-                <OptionTag key={index} value={gender}>{gender}</OptionTag>
-              ))}
-            </SelectTag>
-          </InputContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formTall">身長</LabelTag>
-            <SelectTag id="formTall" value={tall} onChange={(e: ChangeEvent<{ value: unknown }>) => (setTall(e.target.value as number))}>
-              {heights.map((height: string, index: number) => (
-                <OptionTag key={index} value={height}>{height}</OptionTag>
-              ))}
-            </SelectTag>
-          </InputContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formPrefecture">出身地</LabelTag>
-            <SelectTag id="formPrefecture" value={prefecture} onChange={(e: ChangeEvent<HTMLSelectElement>) => (setPrefecture(e.target.value))}>
-              {prefectures.map((prefecture: string, index: number) => (
-                <OptionTag key={index} value={prefecture}>{prefecture}</OptionTag>
-              ))}
-            </SelectTag>
-          </InputContainer>
-          <InputContainer>
-            <LabelTag htmlFor="formIntroduction">自己紹介</LabelTag>
-            <TextAreaTag id="formIntroduction" value={introduction} onChange={(e: ChangeEvent<HTMLTextAreaElement>) => (setIntroduction(e.target.value))}/>
-          </InputContainer>
-          <SubmitButton onClick={onClickPostProfile} disabled={name || age || gender || prefecture || tall || introduction || avatar ? false : true} >送信</SubmitButton>
-        </SignUpContainer>
-      </SignUpWrapper>
+      <MediaQuery query="(min-width: 768px)">
+        <SignUpWrapper>
+          <PageTittle>プロフィール</PageTittle>
+          <SignUpContainer>
+            {job === '演者' ? (
+              <EditPlayerProfile job={job}
+                                name={name}
+                                age={age}
+                                gender={gender}
+                                tall={tall}
+                                prefecture={prefecture}
+                                introduction={introduction}
+                                onChangeImage={onChangeImage} 
+                                onChangeJob={onChangeJob}
+                                onChangeName={onChangeName}
+                                onChangeAge={onChangeAge}
+                                onChangeGender={onChangeGender}
+                                onChangeTall={onChangeTall}
+                                onChangePrefecture={onChangePrefecture}
+                                onChangeIntroduction={onChangeIntroduction}
+              />
+            ) : (
+              <EditCompanyProfile job={job}
+                                  company={company}
+                                  description={description}
+                                  onChangeJob={onChangeJob}
+                                  onChangeCompany={onChangeCompany}
+                                  onChangeDescription={onChangeDescription}
+              />
+            )}
+            <SubmitButton onClick={onClickPostProfile} disabled={name || age || gender || prefecture || tall || introduction || avatar ? false : true} >送信</SubmitButton>
+          </SignUpContainer>
+        </SignUpWrapper>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 767px)">
+        <SignUpWrap>
+          <SignUpContain>
+            <PageTittle>プロフィール</PageTittle>
+            {job === '演者' ? (
+              <EditPlayerProfile job={job}
+                                 name={name}
+                                 age={age}
+                                 gender={gender}
+                                 tall={tall}
+                                 prefecture={prefecture}
+                                 introduction={introduction}
+                                 onChangeImage={onChangeImage} 
+                                 onChangeJob={onChangeJob}
+                                 onChangeName={onChangeName}
+                                 onChangeAge={onChangeAge}
+                                 onChangeGender={onChangeGender}
+                                 onChangeTall={onChangeTall}
+                                 onChangePrefecture={onChangePrefecture}
+                                 onChangeIntroduction={onChangeIntroduction}
+              />
+            ) : (
+              <EditCompanyProfile job={job}
+                                    company={company}
+                                    description={description}
+                                    onChangeJob={onChangeJob}
+                                    onChangeCompany={onChangeCompany}
+                                    onChangeDescription={onChangeDescription}
+              />
+            )}
+            <SubmitButton onClick={onClickPostProfile} disabled={name || age || gender || prefecture || tall || introduction || avatar ? false : true} >送信</SubmitButton>
+          </SignUpContain>
+        </SignUpWrap>
+      </MediaQuery>
+    
     </>
   )
 })
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,51 +1,58 @@
 // サインアップ
 export type SignUpType = {
-  email: string
-  password: string
-  passwordConfirmation: string
+  email: string;
+  password: string;
+  passwordConfirmation: string;
 }
 
 // サインイン
 export type SignInType = {
-  email: string
-  password: string
+  email:    string;
+  password: string;
 }
 
 // ユーザー
 export type User = {
-  id: number
-  uid: string
-  provider: string
-  email: string
-  name?: string
-  nickname?: string
-  image?: string
-  allowPasswordChange: boolean
-  created_at: Date
-  updated_at: Date
+  id: number;
+  uid: string;
+  provider: string;
+  email: string;
+  name?: string;
+  nickname?: string;
+  image?: string;
+  allowPasswordChange: boolean;
+  created_at: Date;
+  updated_at: Date;
 }
 // プロフィール
 export type Profile = {
   id?: number;
-  userId?: number;
+  user_id?: number;
   name?: string;
+  job: string;
   avatar_url: string;
   gender?: string;
   age?: number;
   tall?: number;
   prefecture?: string;
   introduction?: string;
-  created_at?: Date
-  updated_at?: Date
+  company?:      string;
+  description?:  string;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
+// プロフィール更新
 export type ParamsProfile = {
-  id: number | undefined,
-  name: string | undefined,
-  age: number | undefined, 
-  gender: string | undefined,
-  tall: number | undefined, 
-  prefecture: string | undefined,
-  introduction: string | undefined, 
-  avatar: {} | undefined,
+  id:           number | undefined;
+  name:         string | undefined;
+  job:          string;
+  age:          number | undefined;
+  gender:       string | undefined;
+  tall:         number | undefined;
+  prefecture:   string | undefined;
+  introduction: string | undefined;
+  avatar:       {} | undefined;
+  company:      string | undefined;
+  description:  string | undefined;
 }
