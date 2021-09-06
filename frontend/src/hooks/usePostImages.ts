28650d05d569d75ac033e73fa56a0a73c462bfd4
谷村有寿麻 <t.azumax0502@gmail.com>
2021-09-06 20:00:44 +0900

    post機能のカスタムフックス追加

---
+++ frontend/src/hooks/usePostImages.ts
@@ -0,0 +1,35 @@
+import { ChangeEvent, useCallback, useContext, useState } from 'react'
+import { LoginUserContext } from '../App'
+import { paramsPost, Post } from '../types'
+
+export const usePostImages = () => {
+  const [content, setContent] = useState('')
+  const [image, setImage]     = useState({data: '', name: ''})
+  const { currentUser } = useContext(LoginUserContext)
+  
+  const params: paramsPost = {
+    content: content,
+    image: image,
+    id: currentUser?.id
+  }
+
+  const post: Post = {
+    content: content,
+    image_url: image.data,
+  }
+
+  const onChangeImage = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const reader = new FileReader()
+      const file = e.target.files[0];
+      reader.onload = () => { 
+        setImage({
+          data: reader.result as string,
+          name: file.name
+        })
+      }
+      reader.readAsDataURL(file)
+    }
+  }, [setImage])
+  return { content, setContent, image, params, onChangeImage, post }
+}
