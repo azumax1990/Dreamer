62845f4e47fcfb10b546288ab2da3f6cfbf35eca
谷村有寿麻 <t.azumax0502@gmail.com>
2021-09-10 18:13:43 +0900

    image添付機能追加

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,4 +1,10 @@
 class Audition < ApplicationRecord
+  include Rails.application.routes.url_helpers
 
   belongs_to :user
+  has_one_attached :image
+
+  def image_url
+    image.attached? ? url_for(image) : nil
+  end
 end
