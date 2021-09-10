ebc5876c7844a48e88d96179a963796d360e0977
谷村有寿麻 <t.azumax0502@gmail.com>
2021-09-10 18:13:11 +0900

    image添付機能追加

--- app/controllers/api/auditions_controller.rb
+++ app/controllers/api/auditions_controller.rb
@@ -2,12 +2,23 @@ class ::Api::AuditionsController < ApplicationController
 
   def index
     auditions = Audition.all.order(id: "DESC")
-    render json: auditions
+    render json: auditions, methods: [:image_url]
+  end
+
+  def show
+    audition = Audition.find(params[:id])
+    profile = audition.user.profile
+    render json: {audition: audition, profile: profile}
   end
 
   def create
     user = User.find(params[:id])
-    user.auditions.create(title: params[:title], description: params[:description])
+    audition = user.auditions.new(title: params[:title], description: params[:description])
+    if params[:image][:data] != ""
+      audition.image.attach(io: StringIO.new(decode(params[:image][:data]) + "\n"),
+                            filename: params[:image][:name])
+    end
+    audition.save
     render json: { status: 'ok' }
   end
 
@@ -18,8 +29,12 @@ class ::Api::AuditionsController < ApplicationController
   def destroy
   end
 
+  def decode(str)
+    Base64.decode64(str.split(',').last)
+  end
+
   private
   def audition_params
-    params.require(:audition).permit(:title, :description, :id)
+    params.require(:audition).permit(:title, :description, :id, :image)
   end
 end
