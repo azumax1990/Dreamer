859b6b33b8150dfcb2e73e63ca361ecf0c39c005
谷村有寿麻 <t.azumax0502@gmail.com>
2021-09-25 21:39:43 +0900

    avatar_url追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,12 @@
 class Group < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  
   has_many :messages, dependent: :destroy
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
+  has_one_attached :avatar
+
+  def avatar_url
+    avatar.attached? ? url_for(avatar) : nil
+  end
 end
