6314de124a8e92c6cda09dc31ab868332998749a
谷村有寿麻 <t.azumax0502@gmail.com>
2021-09-26 23:00:47 +0900

    リファクタリング

--- frontend/src/components/organisms/profile/pcResponsive/ModalMessages.tsx
+++ frontend/src/components/organisms/profile/pcResponsive/ModalMessages.tsx
@@ -1,6 +1,7 @@
-import React, { memo, useState, VFC } from 'react'
+import React, { memo, useContext, VFC } from 'react'
 import styled from 'styled-components'
-import { Profile, Group, Message } from '../../../../types'
+import { LoginUserContext } from '../../../../App'
+import { Profile, Group, Message, GroupUser } from '../../../../types'
 import { ModalMessage } from '../../message/pcResponsive/ModalMessage'
 
 const ModalWrapper = styled.div`
@@ -30,28 +31,34 @@ const MessagesContainer = styled.div`
   height: 500px;
   overflow: scroll;
 `
-
-
 type Props = {
   profile:                 Profile | undefined;
   groups:                  Array<Group>;
   messages:                Array<Message>;
   profiles:                Array<Profile>;
+  groupUsers:              Array<GroupUser>;
   ChangeMessageModalFalse: () => void;
 }
 
 export const ModalMessages: VFC<Props> = memo((props) => {
-  const { ChangeMessageModalFalse, profile, groups, messages, profiles } = props;
+  const { ChangeMessageModalFalse, profile, groups, messages, profiles, groupUsers } = props;
+  const { currentUser } = useContext(LoginUserContext)
+  
   return (
     <>
       <ModalWrapper onClick={ChangeMessageModalFalse}>
         <OverLay>
           <Modal>
-            <UseName>{profile?.name}</UseName>
+            {profile?.job === '演者' ? (
+              <UseName>{profile?.name}</UseName>
+            ) : (
+              <UseName>{profile?.company}</UseName>
+            )}
             <MessagesContainer>
               {groups.map((group, index) => {
-                const lastMessage = messages.find((message) => message.group_id === group.id)
-                const selectedProfile = profiles.find((profile) => profile?.user_id === lastMessage?.user_id)
+                const selectedUser    = groupUsers.find((groupUser) => groupUser.group_id === group.id && groupUser.user_id !== currentUser?.id)
+                const lastMessage     = messages.find((message) => message.group_id === group.id)
+                const selectedProfile = profiles.find((profile) => profile?.user_id === selectedUser?.user_id)
                 return (
                   <ModalMessage group={group} selectedProfile={selectedProfile} lastMessage={lastMessage} index={index}/>
                 )
