f6fcb69cabba894c24fb12173cdb11a17a0b991a
谷村有寿麻 <t.azumax0502@gmail.com>
2021-10-04 17:37:46 +0900

    スマホ表示からのログアウト機能追加

--- frontend/src/components/organisms/header/smartPhoneResponsive/TopPageSmartPhoneHeader.tsx
+++ frontend/src/components/organisms/header/smartPhoneResponsive/TopPageSmartPhoneHeader.tsx
@@ -1,13 +1,29 @@
 import React, { memo, useContext, VFC } from 'react'
+import Cookies from 'js-cookie'
 import { Link } from 'react-router-dom'
+import { signOut } from '../../../../api/auth'
 import { LoginUserContext } from '../../../../App'
 
 import { LinkText } from '../../../atoms/header/smartPhone/LinkText'
 import { SmartPhoneHeader } from '../../../Molecules/header/smartPhone/SmartPhoneHeader'
 
 export const TopPageSmartPhoneHeader: VFC = memo(() => {
-  const { currentUser } = useContext(LoginUserContext)
+  const { currentUser, setCurrentUser } = useContext(LoginUserContext)
 
+  // ログアウト&Cookie削除
+  const submitSignOut = () => {
+    signOut()
+    .then((res) => {
+      if (res.data.success === true) {
+        Cookies.remove("access_token")
+        Cookies.remove("client")
+        Cookies.remove("uid")
+        setCurrentUser(undefined)
+        alert("ログアウトしました")
+      } 
+    })
+    .catch(() => alert("ログアウト出来ませんでした"))
+  }
   return (
     <SmartPhoneHeader>
       {currentUser ? (
@@ -18,7 +34,7 @@ export const TopPageSmartPhoneHeader: VFC = memo(() => {
           <Link to="/audition" style={{ textDecoration: "none", color: "black" }}>
             <LinkText>募集する</LinkText>
           </Link>
-          <LinkText>ログアウト</LinkText>
+          <LinkText onClick={submitSignOut}>ログアウト</LinkText>
         </>
       ) : (
         <>
