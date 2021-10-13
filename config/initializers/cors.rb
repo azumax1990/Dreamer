cf6e24a5300f9986e23791f5d40e79b5c2af7905
谷村有寿麻 <t.azumax0502@gmail.com>
2021-10-14 04:32:29 +0900

    cors変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://dreamer-test.herokuapp.com'
+    origins 'localhost:3001', 'https://dreamer-test.herokuapp.com'
 
     resource '*',
       headers: :any,
